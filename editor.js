35aa05f3c602d85821fd4382681b6ae0386084c8
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-05-16 19:37:43 +0200

    Added debounce, startShader e title

--- editor.js
+++ editor.js
@@ -1,21 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     let input = document.getElementById("editor");
-    input.addEventListener("keydown", write);
+    input.addEventListener("input", debounce(startShader));
 });
 
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
 function write(){
-    
     document.getElementById("text").innerHTML=(new Date()).getMilliseconds();
 }
 
 
 function removeLive(){
     let input = document.getElementById("editor");
-    input.removeEventListener("keydown", write);
+    input.addEventListener("input", debounce(write));
 }
 
 function addLive(){
     let input = document.getElementById("editor");
-    input.removeEventListener("keydown", write);
-    input.addEventListener("keydown", write);
+    input.removeEventListener("input", debounce(write));
+    input.addEventListener("input", debounce(write));
+}
+
+function startShader(){
+    const canvas = document.getElementById("shaderCanvas");
+    const gl = canvas.getContext("webgl");
+    if (gl === null) {alert("Unable to initialize WebGL. Your browser or machine may not support it.",)}
+      // Set clear color to black, fully opaque
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
 }
