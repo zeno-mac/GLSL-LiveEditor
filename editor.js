780f54764314b81bb3f97af1a36d01d96252f661
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-05-23 17:59:16 +0200

    Standard fragShaderSource added

--- editor.js
+++ editor.js
@@ -10,11 +10,25 @@ const vertexShaderSource = `
   }
 `;
 
+const fragmentShaderSource= `
+precision mediump float;
+uniform float uTime;
+uniform vec2 uResolution;
+varying vec2 vUV;
+
+void main() {
+    vec2 uv = vUV;
+    vec3 col = 0.5 + 0.5 * cos(uTime + uv.xyx + vec3(0, 2, 4));
+    gl_FragColor = vec4(col, 1.0);
+}
+`;
+
 const debouncedSendShader = debounce(sendShader, 300);
 let input;
 
 document.addEventListener("DOMContentLoaded", () => {
     input = document.getElementById("editor");
+    input.value=fragmentShaderSource;
     input.addEventListener("input", debouncedSendShader);
     sendShader();
     
