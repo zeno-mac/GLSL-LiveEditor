1ba274018c0455824004a9e5231b06de3e2ea3ee
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-08-05 17:54:50 +0200

    Added two setter button and Mouse Shader

--- editor.js
+++ editor.js
@@ -10,17 +10,40 @@ const vertexShaderSource = `
   }
 `;
 
-const fragmentShaderSource= `
-precision mediump float;
+const standardFragmentShaderSource= 
+`precision mediump float;
 uniform float uTime;
 uniform vec2 uResolution;
 varying vec2 vUV;
 
 void main() {
-    vec2 uv = vUV;
-    vec3 col = 0.5 + 0.5 * cos(uTime + uv.xyx + vec3(0, 2, 4));
-    gl_FragColor = vec4(col, 1.0);
+vec2 uv = vUV;
+vec3 col = 0.5 + 0.5 * cos(uTime + uv.xyx + vec3(0, 2, 4));
+gl_FragColor = vec4(col, 1.0);
 }
+`;
+
+const mouseFragmentShaderSource=
+`precision mediump float;
+uniform vec2 uResolution;
+uniform vec2 uMouse;
+uniform float uTime;
+
+void main() {
+vec2 uv = gl_FragCoord.xy;
+
+float dist = distance(uv, uMouse);
+vec3 col = vec3(0.,0.,0.);    
+float radius = 30.0;
+float relDist = 1.0 -dist/radius;
+
+if(dist <radius){
+col= vec3(relDist*abs(cos(uTime)),relDist*abs(sin(uTime)),0);}
+
+gl_FragColor = vec4(col, 1.0);
+
+}
+
 `;
 
 const debouncedSendShader = debounce(sendShader, 300);
@@ -28,16 +51,26 @@ let input;
 
 document.addEventListener("DOMContentLoaded", () => {
     input = document.getElementById("editor");
-    input.value=fragmentShaderSource;
+    input.value=standardFragmentShaderSource;
     input.addEventListener("input", debouncedSendShader);
     sendShader();
     
     document.getElementById("Compile").addEventListener("click", sendShader);
     document.getElementById("AddLive").addEventListener("click", removeLiveCompiling);
     document.getElementById("RemoveLive").addEventListener("click", addLiveCompiling);
+    document.getElementById("SetStandardShader").addEventListener("click", setStandardShader);
+    document.getElementById("SetMouseShader").addEventListener("click", setMouseShader);
 });
 
+function setStandardShader(){
+  input.value=standardFragmentShaderSource;
+  sendShader();
+}
 
+function setMouseShader(){
+  input.value=mouseFragmentShaderSource;
+  sendShader();
+}
 
 function removeLiveCompiling(){
     input.removeEventListener("input", debouncedSendShader);
