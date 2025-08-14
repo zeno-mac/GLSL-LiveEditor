ba6e4e713a7dd621feced3f7482aec82b82c6001
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-08-14 18:01:36 +0200

    Added an Event for Tab and Shift+Tab

--- editor.js
+++ editor.js
@@ -10,8 +10,8 @@ const vertexShaderSource = `
   }
 `;
 
-const standardFragmentShaderSource= 
-`precision mediump float;
+const standardFragmentShaderSource =
+  `precision mediump float;
 uniform float uTime;
 uniform vec2 uResolution;
 varying vec2 vUV;
@@ -23,8 +23,8 @@ gl_FragColor = vec4(col, 1.0);
 }
 `;
 
-const mouseFragmentShaderSource=
-`precision mediump float;
+const mouseFragmentShaderSource =
+  `precision mediump float;
 uniform vec2 uResolution;
 uniform vec2 uMouse;
 uniform float uTime;
@@ -50,61 +50,82 @@ const debouncedSendShader = debounce(sendShader, 300);
 let input;
 let liveCompiling = true;
 document.addEventListener("DOMContentLoaded", () => {
-    input = document.getElementById("editor");
-    input.value=standardFragmentShaderSource;
-    input.addEventListener("input", debouncedSendShader);
-    sendShader();
-    document.getElementById("Compile").addEventListener("click", sendShader);
-    document.getElementById("ToggleLive").classList.add("live-active");
-    document.getElementById("ToggleLive").addEventListener("click", toggleLiveCompiling);
-    document.getElementById("SetStandardShader").addEventListener("click", setStandardShader);
-    document.getElementById("SetMouseShader").addEventListener("click", setMouseShader);
-    document.getElementById("DownloadShader").addEventListener("click", downloadShader);
-    document.getElementById("LoadShader").addEventListener("click", () => {
-        document.getElementById("LoadedShaderFile").click();
-    });
-    document.getElementById("LoadedShaderFile").addEventListener("change", loadShader);
+  input = document.getElementById("editor");
+  input.value = standardFragmentShaderSource;
+  input.addEventListener("input", debouncedSendShader);
+  sendShader();
+  document.getElementById("Compile").addEventListener("click", sendShader);
+  document.getElementById("ToggleLive").classList.add("live-active");
+  document.getElementById("ToggleLive").addEventListener("click", toggleLiveCompiling);
+  document.getElementById("SetStandardShader").addEventListener("click", setStandardShader);
+  document.getElementById("SetMouseShader").addEventListener("click", setMouseShader);
+  document.getElementById("DownloadShader").addEventListener("click", downloadShader);
+  document.getElementById("LoadShader").addEventListener("click", () => {
+    document.getElementById("LoadedShaderFile").click();
+  });
+  input.addEventListener('keydown', (e) => {
+    if (e.key === "Tab") {
+      let start = e.target.selectionStart;
+      let end = e.target.selectionEnd;
+      let target = e.target;
+      let value = target.value;
+      if (e.shiftKey) {
+        target.value = value.substring(0, start - 4)
+          + value.substring(end);
+        e.target.selectionStart = e.target.selectionEnd = start - 4;
+      }
+      else {
+        target.value = value.substring(0, start)
+          + "    "
+          + value.substring(end);
+        e.target.selectionStart = e.target.selectionEnd = start + 4;
+      }
+      e.preventDefault();
+      debouncedSendShader();
+    }
+  }, false);
+  document.getElementById("LoadedShaderFile").addEventListener("change", loadShader);
 });
 
-function loadShader(){
+function loadShader() {
   const file = document.getElementById("LoadedShaderFile").files[0];
   const reader = new FileReader();
-  reader.readAsText(file); 
-  
+  reader.readAsText(file);
+
   reader.onload = (e) => {
     input.value = e.target.result;
     sendShader();
   };
 }
 
-function downloadShader(){
-const link = document.createElement("a");
-const content = input.value;
-const file = new Blob([content], { type: 'text/plain' });
-link.href = URL.createObjectURL(file);
-link.download = "shader.frag";
-link.click();
-URL.revokeObjectURL(link.href);
+function downloadShader() {
+  const link = document.createElement("a");
+  const content = input.value;
+  const file = new Blob([content], { type: 'text/plain' });
+  link.href = URL.createObjectURL(file);
+  link.download = "shader.frag";
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
 
-function setStandardShader(){
-  input.value=standardFragmentShaderSource;
+function setStandardShader() {
+  input.value = standardFragmentShaderSource;
   sendShader();
 }
 
-function setMouseShader(){
-  input.value=mouseFragmentShaderSource;
+function setMouseShader() {
+  input.value = mouseFragmentShaderSource;
   sendShader();
 }
 
-function toggleLiveCompiling(){
+function toggleLiveCompiling() {
   let button = document.getElementById("ToggleLive");
-  if (liveCompiling){
+  if (liveCompiling) {
     input.removeEventListener("input", debouncedSendShader);
     button.classList.remove("live-active");
     button.classList.add("live-inactive");
     liveCompiling = false;
-  }else{
+  } else {
     input.addEventListener("input", debouncedSendShader);
     button.classList.remove("live-inactive");
     button.classList.add("live-active");
@@ -112,12 +133,12 @@ function toggleLiveCompiling(){
   }
 }
 
-function sendShader(){
+function sendShader() {
   const fragText = input.value;
   startShader(vertexShaderSource, fragText);
 }
 
-function debounce(func, timeout = 300){
+function debounce(func, timeout = 300) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
